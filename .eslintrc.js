0e131e2cb7bd85b1d736a7e36e8f26a512bb1175
Paul Irish <paul.irish@gmail.com>
2016-12-13 13:52:59 -0800

    reorder of eslintrc + comments.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
+  // start with google standard style
+  //     https://github.com/google/eslint-config-google/blob/master/index.js
   "extends": "google",
   "env": {
     "node": true
   },
   "rules": {
+    // 2 == error, 1 == warning, 0 == off
     "max-len": [2, 100, {
       "ignoreComments": true,
       "ignoreUrls": true,
@@ -25,14 +28,16 @@ module.exports = {
       "varsIgnorePattern": "(^_$)"
     }],
     "quotes": [2, "single"],
+    "strict": [2, "global"],
+    "prefer-const": 2,
+
+    // Disabled rules
     "require-jsdoc": 0,
     "valid-jsdoc": 0,
     "comma-dangle": 0,
-    "prefer-const": 2,
+    "arrow-parens": 0,
     // Compat: support for rest params is behind a flag for node v5.x
     "prefer-rest-params": 0,
-    "arrow-parens": 0,
-    "strict": [2, "global"]
   },
   "parserOptions": {
     "ecmaVersion": 6,
